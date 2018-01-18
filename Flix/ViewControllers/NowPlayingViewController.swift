eb7db47e1f37a7f38ae759d1145f4423c0551a2d
xhpwn <saxena20@purdue.edu>
2018-01-17 22:39:09 -0500

    Lab 2 Story #1 x, Tab bar added

--- Flix/ViewControllers/NowPlayingViewController.swift
+++ Flix/ViewControllers/NowPlayingViewController.swift
@@ -88,4 +88,20 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
         return cell
     }
     
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let cell = sender as! UITableViewCell
+        if let indexPath = tableView.indexPath(for: cell) {
+            let movie = movies[indexPath.row]
+            let detailViewController = segue.destination as! DetailViewController
+            detailViewController.movie = movie
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
 }
